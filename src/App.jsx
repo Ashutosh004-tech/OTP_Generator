0fe9dfdb311390db2dc525046306019f3d5262e3
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-06-21 08:13:44 +0530

    Css Style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.css";
 export default function App() {
-    
   return (
     <div className="usersignUp">
       <h2>Enter your details</h2>
       <form action="" className="userForm">
-        <div>
-          <label htmlFor="userName"> User name:</label>
-          <input type="text" id="userName" />
-        </div>
         <div>
           <label htmlFor="userEmail"> Enter email id:</label>
           <input type="text" id="userEmail" />
@@ -16,13 +11,11 @@ export default function App() {
         <div>
           <label htmlFor="userOTP">Enter OTP:</label>
           <input type="text" id="userOTP" />
-          <button>Generate</button>
         </div>
         <div>
-          <label htmlFor="userPassword">Enter Password:</label>
-          <input type="text" id="userPassword" />
+          <button>Generate</button>
+          <button>Submit</button>
         </div>
-        <button>Submit</button>
       </form>
     </div>
   );
