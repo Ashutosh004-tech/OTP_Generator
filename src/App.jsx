63fe9dec8b0b5221b12981b9da86c8c4af409346
Ashutosh Panigrahi <ashutoshpanigrahi11@gmail.com>
2026-06-22 07:27:21 +0530

    Otp generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 export default function App() {
+  const [isGeterated, setGenerated] = useState(false);
+  let generateOTP = (e) => {
+    e.preventDefault();
+    setGenerated(true);
+    console.log(Math.floor(1000 + Math.random() * 9000));
+  };
+
+  let verifyOTP = (e) => {
+     e.preventDefault();
+    console.log("Verified ")
+  };
   return (
     <div className="usersignUp">
       <h2>Enter your details</h2>
@@ -8,12 +20,13 @@ export default function App() {
           <label htmlFor="userEmail"> Enter email id:</label>
           <input type="text" id="userEmail" />
         </div>
+
         <div>
           <label htmlFor="userOTP">Enter OTP:</label>
           <input type="text" id="userOTP" />
         </div>
         <div>
-          <button>Generate</button>
+          {isGeterated ? <button onClick={verifyOTP}>Verify</button>: <button onClick={generateOTP}>Generate</button>}
           <button>Submit</button>
         </div>
       </form>
